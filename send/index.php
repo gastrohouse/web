e35ee3d363c7875b109c691b5e71f284cad7798e
zabatonni <zabatonni@gmail.com>
2018-05-04 10:21:05 +0200

    added script for handling form requests

--- send/index.php
+++ send/index.php
@@ -1,5 +1,35 @@
 ---
 ---
-
 <?php
-var_dump('ide');
+
+if($_SERVER['REQUEST_METHOD']!='POST') { exit('nothing to show'); }
+
+$dir=getcwd().'/';
+$filename='emails.csv';
+$filepath=$dir.$filename;
+
+$data['time']=date('c',time());
+$data['meno']=$_POST['meno'] ?? null;
+$data['email']=$_POST['email'] ?? null;
+$data['sprava']=$_POST['sprava'] ?? null;
+$data['ip']=$_SERVER['REMOTE_ADDR'];
+$data['browser']=$_SERVER['HTTP_USER_AGENT'];
+
+$file=fopen($filepath,'a+');
+
+flock($file,LOCK_EX);
+fputcsv($file,$data);
+flock($file,LOCK_UN);
+fclose($file);
+
+mail(
+	'zabatonni@gmail.com',
+	'Napiste nam',
+	json_encode($data),
+	array(
+		'From'=>'admin@gastrohouse.sk',
+	)
+);
+
+header('Location: https://gastrohouse.sk/sent/');
+exit;
